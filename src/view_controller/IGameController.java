2a12cf976819688e94a48c6d81d0aaf894093daa
davidog23 <davidolmos23@gmail.com>
2016-01-17 22:15:20 +0100

    Stuff

---
+++ src/view_controller/IGameController.java
@@ -0,0 +1,12 @@
+package view_controller;
+
+import model.Jugador;
+
+/**
+ * Interface para controllers.
+ * Created by David on 13/01/2016.
+ */
+public interface IGameController {
+
+    public void setPlayers(Jugador player, Jugador target);
+}
